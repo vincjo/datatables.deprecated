06ec15cb8ac3802dc6662d19d6fb88ccf7b49342
vincjo <v.joseph3@gmail.com>
2020-07-05 16:04:33 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Datatable from "./Datatable.svelte"
 import PaginationButtons from "./PaginationButtons.svelte"
 import PaginationRowCount from "./PaginationRowCount.svelte"
 import SearchInput from "./SearchInput.svelte"
-import rows from "./store.js"
+import { rows } from "./store.js"
 
 export {
   Datatable,
