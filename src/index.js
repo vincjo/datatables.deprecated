a33a68400c118cc204b861413ddef4343f8eb196
vincjo <v.joseph3@gmail.com>
2020-10-31 13:06:50 +0100

    0.1.10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Datatable from "./Datatable.svelte"
-import Key from "./Key.svelte"
 import PaginationButtons from "./PaginationButtons.svelte"
 import PaginationRowCount from "./PaginationRowCount.svelte"
 import SearchInput from "./SearchInput.svelte"
@@ -7,7 +6,6 @@ import { rows } from "./stores/data.js"
 
 export {
   Datatable,
-  Key,
   PaginationButtons,
   PaginationRowCount,
   SearchInput,
