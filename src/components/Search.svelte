2e919a46b1a8dbad68ab5aa2074cb1ac54a65412
pa-aaron <aaron.mcclinchey@provision.io>
2020-11-11 14:25:04 -0700

    replace <main> with <section> to avoid conflicts; applied formatting including CSS expansion

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,12 +1,14 @@
 <script>
-    import { options } from '../stores/options.js'
-    import SearchInput from '../SearchInput.svelte'
+  import SearchInput from "../SearchInput.svelte";
 </script>
 
 <section class="dt-search">
-    <SearchInput/>
+  <SearchInput />
 </section>
 
 <style>
-    section{height:32px;margin-left:16px;}
+  section {
+    height: 32px;
+    margin-left: 16px;
+  }
 </style>
