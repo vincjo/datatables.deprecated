3830b685742eab7787d0bcc1b87456f2953548c0
Richard Davenport <richard.davenport@mbpnetwork.com>
2022-07-15 17:31:42 -0500

    Add missing types

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import Datatable from './Datatable.svelte'
+import PaginationButtons from './PaginationButtons.svelte'
+import PaginationRowCount from './PaginationRowCount.svelte'
+import SearchInput from './SearchInput.svelte'
+import ColumnFilterInputs from './ColumnFilterInputs.svelte'
+
+export {
+    Datatable,
+    PaginationButtons,
+    PaginationRowCount,
+    SearchInput,
+    ColumnFilterInputs,
+}
